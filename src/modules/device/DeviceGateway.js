73ac538b2f30e2f2d9ef2b488813c0cc4aaf0fbd
Bruceliubeiru <109455434+Bruceliubeiru@users.noreply.github.com>
2026-06-21 17:32:12 +0800

    feat(device): delegate gateway calls to adapter

--- src/modules/device/DeviceGateway.js
+++ src/modules/device/DeviceGateway.js
@@ -1,8 +1,27 @@
+const DeviceAdapter = require("./adapters/DeviceAdapter");
+
 class DeviceGateway {
   constructor(options = {}) {
     this.provider = options.provider || "mock";
     this.adapter = options.adapter || null;
   }
+
+  getAdapter() {
+    if (!this.adapter) {
+      throw new Error("DeviceGateway adapter is not configured");
+    }
+    return this.adapter;
+  }
 }
+
+DeviceAdapter.DEVICE_METHODS.forEach((methodName) => {
+  DeviceGateway.prototype[methodName] = async function proxy(input = {}) {
+    const adapter = this.getAdapter();
+    if (typeof adapter[methodName] !== "function") {
+      throw new Error(`Device adapter does not implement ${methodName}`);
+    }
+    return adapter[methodName](input);
+  };
+});
 
 module.exports = DeviceGateway;
