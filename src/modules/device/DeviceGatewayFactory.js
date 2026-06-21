0a7bd19bdc94e504ee4afe02d353ce9b5bcf4dab
Bruceliubeiru <109455434+Bruceliubeiru@users.noreply.github.com>
2026-06-21 17:19:29 +0800

    chore: use mock adapter by default

--- src/modules/device/DeviceGatewayFactory.js
+++ src/modules/device/DeviceGatewayFactory.js
@@ -1,10 +1,10 @@
 const DeviceGateway = require("./DeviceGateway");
+const MockMoyuntengAdapter = require("./adapters/MockMoyuntengAdapter");
 
 function createDeviceGateway(options = {}) {
-  return new DeviceGateway({
-    provider: options.provider || "mock",
-    adapter: options.adapter || null,
-  });
+  const provider = options.provider || "mock";
+  const adapter = options.adapter || new MockMoyuntengAdapter({ provider });
+  return new DeviceGateway({ provider, adapter });
 }
 
 module.exports = {
