dcf9fa5add7e40ca70ec4f5bfcb79e7e944cc20e
Bruceliubeiru <109455434+Bruceliubeiru@users.noreply.github.com>
2026-06-21 17:35:58 +0800

    chore: make moyunteng adapter valid js shell

--- src/modules/device/adapters/MoyuntengAdapter.js
+++ src/modules/device/adapters/MoyuntengAdapter.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const DeviceAdapter = require("./DeviceAdapter");
+
+class MoyuntengAdapter extends DeviceAdapter {
+  constructor(options = {}) {
+    super({ ...options, provider: "moyunteng" });
+  }
+}
+
+module.exports = MoyuntengAdapter;
