f00db8f48430008549c28f6aba0223a3929e4a5c
Bruceliubeiru <109455434+Bruceliubeiru@users.noreply.github.com>
2026-06-21 17:29:54 +0800

    chore: make cloud phone adapter valid js

--- src/modules/device/adapters/CloudPhoneAdapter.js
+++ src/modules/device/adapters/CloudPhoneAdapter.js
@@ -1,1 +1,9 @@
-placeholder
+const DeviceAdapter = require("./DeviceAdapter");
+
+class CloudPhoneAdapter extends DeviceAdapter {
+  constructor(options = {}) {
+    super({ ...options, provider: "cloud-phone" });
+  }
+}
+
+module.exports = CloudPhoneAdapter;
